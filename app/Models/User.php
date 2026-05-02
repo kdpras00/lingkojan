bb3b6b5dbaa1e30c58e3b9c03e8d1c9f40fe65de
kdpras00 <kdpras00@gmail.com>
2026-05-02 20:02:31 +0700

    refactor: replace attribute-based mass assignment and hidden properties with standard class properties in User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,19 +4,45 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Database\Factories\UserFactory;
-use Illuminate\Database\Eloquent\Attributes\Fillable;
-use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
 
-#[Fillable(['name', 'email', 'password', 'username', 'nik', 'phone', 'rt', 'rw', 'address', 'gender', 'birth_date', 'birth_place'])]
-#[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable, HasRoles;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'username',
+        'nik',
+        'phone',
+        'rt',
+        'rw',
+        'address',
+        'gender',
+        'birth_date',
+        'birth_place',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
 
     /**
      * Get the attributes that should be cast.
